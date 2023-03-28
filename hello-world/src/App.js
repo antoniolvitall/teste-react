7f49524bd01e5cd9541803548d04199b076aeaa4
Antonio Lindo <antoniolvitall@gmail.com>
2023-03-27 23:15:09 -0300

    teste

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -3,10 +3,12 @@ import FirstComponent from './components/FirstComponent'
 import TemplateExpressions from './components/TemplateExpressions';
 import MyComponent from './components/MyComponent';
 import Events from './components/Events';
+import Challenge from './components/Challenge';
 
 // style / css
 
 import './App.css';
+
 
 
 
@@ -19,6 +21,7 @@ function App() {
       <TemplateExpressions />
       <MyComponent />
       <Events />
+      <Challenge/>
     </div>
   );
 }
